3920d1aeb76498dfab1ca58bf153f0a6adf4ad37
shokhtalat <shokhrukhtalatov@gmail.com>
2025-11-26 15:15:45 +0900

    feat: backend setup going on

---
+++ smart-delivery-backend/src/main/java/com/shokhrukh/smart_delivery/SmartDeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shokhrukh.smart_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDeliveryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDeliveryBackendApplication.class, args);
+	}
+
+}
---
+++ smart-delivery-backend/src/test/java/com/shokhrukh/smart_delivery/SmartDeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shokhrukh.smart_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDeliveryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
